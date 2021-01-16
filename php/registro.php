4dfbee4e12746cc01a4b41cd34a96bbdbd7664a5
Yahir Adrian <leonyahir4686@gmail.com>
2021-01-16 16:32:02 -0500

    Diseño de la página

---
+++ php/registro.php
@@ -0,0 +1,7 @@
+<?php
+echo"<img src='../src/cheque.png' alt='correcto'/>";
+
+echo "<script>
+        setTimeout(()=>{ window.location = '../index.html';}, 3000);
+      </script>";
+?>
---
+++ php/sesion.php
@@ -0,0 +1,5 @@
+<?php
+    echo "<script>
+    setTimeout(()=>{ window.location = '../inicio.php';}, 1500);
+    </script>";
+?>
